eedd21917e9b2199cba81deff40c269819be456e
g3rda <gerda.63rd4@gmail.com>
2020-10-04 15:28:55 +0300

    add structure, minor changes'

--- cp_1/kostetska_fb_83_cp1/clear_text.py
+++ cp_1/kostetska_fb_83_cp1/clear_text.py
@@ -1,15 +1,15 @@
 import re
 
 # ask for filename
-print('Enter name of the file to clear: ', end='')
+print('Enter name of the file from directory text/ to clear: ', end='')
 file_to_clear = input()
 
 # read file
-with open(file_to_clear, 'r', encoding='utf-8') as file:
+with open('text/'+file_to_clear, 'r', encoding='utf-8') as file:
     lines = file.readlines();
 
 # replace all chars except Russian letters with spaces
-text = re.sub('[^\u0430-\u044F\u0451 ]', ' ', ''.join(lines))
+text = re.sub('[^\u0430-\u044F\u0451 ]', ' ', ''.join(lines).lower())
 
 # replace some chars and delete unnecessary spaces
 text = text.replace('ъ', 'ь')
@@ -18,6 +18,7 @@ while '  ' in text:
     text = text.replace('  ', ' ')
 
 # write cleared text to file
-with open('cleared_' + file_to_clear, 'w', encoding='utf-8') as file:
+with open('text/cleared_' + file_to_clear, 'w', encoding='utf-8') as file:
     file.write(text)
-print('Text successfully cleared and written to file cleared_' + file_to_clear)
+
+print('Text successfully cleared and written to text/cleared_' + file_to_clear)
--- cp_1/kostetska_fb_83_cp1/task.py
+++ cp_1/kostetska_fb_83_cp1/task.py
@@ -1,88 +1,47 @@
-import math
+from kostetska_fb_83_cp1.functions.functions import *
 
-
-def count_unigrams(text1):
-    frequencies1 = {i: text1.count(i) / len(text1) for i in sorted(set(text1))}
-    return frequencies1
-
-
-def count_bigrams(text2, intersection):
-    bigrams_number = len(text2) - 1 if intersection else len(text2) // 2
-    bigrams = [(i+j) for (i, j) in zip(text2[0::1 if intersection else 2], text2[1::1 if intersection else 2])]
-    frequencies2 = {i: bigrams.count(i) / bigrams_number for i in sorted(set(bigrams))}
-    return frequencies2
-
-
-def entropy(frequencies):
-    h = 0
-    for i in frequencies:
-        h -= frequencies[i] * math.log2(frequencies[i])
-    h = h / len(list(frequencies.keys())[0])
-    return h
-
-
-def redundancy(h, letters_number):
-    return 1 - (h / math.log2(letters_number))
-
-
-def print_bigrams_table(fr, file):
-    letters = 'абвгдежзийклмнопрстуфхцчшщыьэюя '
-    table = [[0.0 for i in range(len(letters))] for i in range(len(letters))]
-    for i in fr:
-        table[letters.index(i[0])][letters.index(i[1])] = fr[i]
-    file.write('   ')
-    for i in letters:
-        file.write('  '+i+'   ')
-    file.write('\n')
-    for i in table:
-        file.write(letters[table.index(i)]+'  ')
-        for j in i:
-            file.write("{:.3f}".format(j*100)+' ')
-        file.write('\n')
-
-
-# print('Print name of the file with text: ', end='')
+# print('Print name of the file from directory text/ with cleared text: ', end='')
 # file_to_open = input()
-file_to_open = 'cleared_text.txt'
+file_to_open = 'cleared_text2.txt'
 
-with open(file_to_open, 'r', encoding='utf-8') as file:
-    text = file.readline()
+with open('text/'+file_to_open, 'r', encoding='utf-8') as file:
+    text = ''.join(file.readlines())
 
 text_no_spaces = text.replace(' ', '')
 
-res_file = open('results.txt', 'w')
+res_file = open('results/results_'+file_to_open, 'w', encoding='utf-8')
 
 
-res_file.write('               FREQUENCIES                         \n')
+res_file.write('                                             FREQUENCIES (in %)\n')
 
 
-res_file.write('---------------unigrams with spaces---------------\n')
+res_file.write('\n\n                                             unigrams with spaces\n')
 fr_uni_with_spaces = count_unigrams(text)
 for c in fr_uni_with_spaces:
-    res_file.write(c + ": " + "{:.3f}".format(fr_uni_with_spaces[c]*100) + '\n')
+    res_file.write(c + ": " + "{:.4f}".format(fr_uni_with_spaces[c]*100) + '\n')
 
-res_file.write('---------------unigrams no spaces---------------\n')
+res_file.write('\n\n                                             unigrams no spaces\n')
 fr_uni_no_spaces = count_unigrams(text_no_spaces)
 for c in fr_uni_no_spaces:
-    res_file.write(c + ": " + "{:.3f}".format(fr_uni_no_spaces[c]*100) + '\n')
+    res_file.write(c + ": " + "{:.4f}".format(fr_uni_no_spaces[c]*100) + '\n')
 
-res_file.write('---------------bigrams with spaces, intersection yes---------------\n')
+res_file.write('\n\n                                             bigrams with spaces, intersection yes\n')
 fr_bi_with_spaces_yes = count_bigrams(text, True)
 print_bigrams_table(fr_bi_with_spaces_yes, res_file)
 
-res_file.write('---------------bigrams no spaces, intersection yes---------------\n')
+res_file.write('\n\n                                             bigrams no spaces, intersection yes\n')
 fr_bi_no_spaces_yes = count_bigrams(text_no_spaces, True)
 print_bigrams_table(fr_bi_no_spaces_yes, res_file)
 
-res_file.write('---------------bigrams with spaces, intersection no---------------\n')
+res_file.write('\n\n                                             bigrams with spaces, intersection no\n')
 fr_bi_with_spaces_no = count_bigrams(text, False)
 print_bigrams_table(fr_bi_with_spaces_no, res_file)
 
-res_file.write('---------------bigrams no spaces, intersection no---------------\n')
+res_file.write('\n\n                                             bigrams no spaces, intersection no\n')
 fr_bi_no_spaces_no = count_bigrams(text_no_spaces, False)
 print_bigrams_table(fr_bi_no_spaces_no, res_file)
 
-res_file.write('                                         ENTROPY                         \n')
+res_file.write('\n\n                                                      ENTROPY (in bits)\n\n')
 res_file.write('Unigrams with spaces:                                    '+str(entropy(fr_uni_with_spaces))+'\n')
 res_file.write('Unigrams no spaces:                                      '+str(entropy(fr_uni_no_spaces))+'\n')
 res_file.write('Bigrams with spaces, intersection yes:                   '+str(entropy(fr_bi_with_spaces_yes))+'\n')
@@ -90,13 +49,16 @@ res_file.write('Bigrams no spaces, intersection yes:                     '+str(e
 res_file.write('Bigrams with spaces, intersection no:                    '+str(entropy(fr_bi_with_spaces_no))+'\n')
 res_file.write('Bigrams no spaces, intersection no:                      '+str(entropy(fr_bi_no_spaces_no))+'\n')
 
-res_file.write('                                         REDUNDANCY                         \n')
-res_file.write('Unigrams with spaces:                                    '+str(redundancy(entropy(fr_uni_with_spaces), 32))+'\n')
-res_file.write('Unigrams no spaces:                                      '+str(redundancy(entropy(fr_uni_no_spaces), 31))+'\n')
-res_file.write('Bigrams with spaces, intersection yes:                   '+str(redundancy(entropy(fr_bi_with_spaces_yes), 32))+'\n')
-res_file.write('Bigrams no spaces, intersection yes:                     '+str(redundancy(entropy(fr_bi_no_spaces_yes), 31))+'\n')
-res_file.write('Bigrams with spaces, intersection no:                    '+str(redundancy(entropy(fr_bi_with_spaces_no), 32))+'\n')
-res_file.write('Bigrams no spaces, intersection no:                      '+str(redundancy(entropy(fr_bi_no_spaces_no), 31))+'\n')
+res_file.write('\n\n                                                      REDUNDANCY\n\n')
+res_file.write('Unigrams with spaces:                                    '+"{:.3f}".format(redundancy(entropy(fr_uni_with_spaces), len(set(''.join(fr_uni_with_spaces))))*100)+'%\n')
+res_file.write('Unigrams no spaces:                                      '+"{:.3f}".format(redundancy(entropy(fr_uni_no_spaces), len(set(''.join(fr_uni_no_spaces))))*100)+'%\n')
+res_file.write('Bigrams with spaces, intersection yes:                   '+"{:.3f}".format(redundancy(entropy(fr_bi_with_spaces_yes), len(set(''.join(fr_bi_with_spaces_yes))))*100)+'%\n')
+res_file.write('Bigrams no spaces, intersection yes:                     '+"{:.3f}".format(redundancy(entropy(fr_bi_no_spaces_yes), len(set(''.join(fr_bi_no_spaces_yes))))*100)+'%\n')
+res_file.write('Bigrams with spaces, intersection no:                    '+"{:.3f}".format(redundancy(entropy(fr_bi_with_spaces_no), len(set(''.join(fr_bi_with_spaces_no))))*100)+'%\n')
+res_file.write('Bigrams no spaces, intersection no:                      '+"{:.3f}".format(redundancy(entropy(fr_bi_no_spaces_no), len(set(''.join(fr_bi_no_spaces_no))))*100)+'%\n')
 
+res_file.write('CoolPinkProgram H(10):                                   '+"{:.3f}".format(redundancy(3.15148903446785,32))+'<R<'+"{:.3f}".format(redundancy(2.25684412683459, 32))+'\n')
+res_file.write('CoolPinkProgram H(20):                                   '+"{:.3f}".format(redundancy(2.7164756600148,32))+'<R<'+"{:.3f}".format(redundancy(1.84869890253276, 32))+'\n')
+res_file.write('CoolPinkProgram H(30):                                   '+"{:.3f}".format(redundancy(2.61598090820483,32))+'<R<'+"{:.3f}".format(redundancy(1.6863475402192, 32))+'\n')
 
 res_file.close()
