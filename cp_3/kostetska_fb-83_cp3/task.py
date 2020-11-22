a29740df264d0fdef4f1e794fb8d78a8f0085d06
g3rda <gerda.63rd4@gmail.com>
2020-11-22 22:11:01 +0200

    add criteria

--- cp_3/kostetska_fb-83_cp3/task.py
+++ cp_3/kostetska_fb-83_cp3/task.py
@@ -1,14 +1,26 @@
 from collections import Counter
+import itertools
+import math
 
 alphabet = "абвгдежзийклмнопрстуфхцчшщыьэюя"
+
+def count_unigrams(text1):
+    frequencies1 = {i: text1.count(i) / len(text1) for i in sorted(set(text1))}
+    return frequencies1
 
 def count_bigrams(text, intersection):
     bigrams_number = len(text2) - 1 if intersection else len(text) // 2
     bigrams = [(i + j) for (i, j) in zip(text[0::1 if intersection else 2], text[1::1 if intersection else 2])]
     coun = Counter(bigrams)
-    frequencies = {i: coun[i] / bigrams_number for i in coun}
-    return frequencies
+    frequencies2 = {i: coun[i] / bigrams_number for i in coun}
+    return frequencies2
 
+def entropy(frequencies):
+    h = 0
+    for i in frequencies:
+        h -= frequencies[i] * math.log2(frequencies[i])
+    h = h / len(list(frequencies.keys())[0])
+    return h
 
 def euclid(a, b):
     if b==0:
@@ -37,32 +49,61 @@ def solve_linear(a, b, m):
     if gcd==1:
         return [inverse_element(a, m)*b%m]
     elif gcd>1:
-        if b%d!=0:
+        if b%gcd!=0:
             return []
         else:
-            x0=b/d*inverse_element(a/d)%m
-            return [x0+m/d*i for i in range(d)]
+            x0=(b/gcd*inverse_element(a/gcd, m))%m
+            return [x0+m/gcd*i for i in range(gcd)]
 
 def letters_from_upper_letter(x):
     return alphabet[int((x-x%len(alphabet))/len(alphabet))]+alphabet[x%len(alphabet)]
 
 def decipher_afin_bigrams(a, b, text):
+    out = []
     bigrams = [(i + j) for (i, j) in zip(text[0::2], text[1::2])]
     for i in bigrams:
-        x = inverse_element(a, pow(len(alphabet),2))*(upper_letter(i[0], i[1])-b)%pow(len(alphabet),2)
-        print(letters_from_upper_letter(x), end='')
+        inv = inverse_element(a, pow(len(alphabet),2))
+        if inv!=None:
+            x = int(inv*(upper_letter(i[0], i[1])-b)%pow(len(alphabet),2))
+            out.append(letters_from_upper_letter(x))
+    return ''.join(out)
+
+def get_coefs(a, b):
+    coefa = solve_linear(upper_letter(a[0][0], a[0][1]) - upper_letter(a[1][0], a[1][1]), upper_letter(b[0][0], b[0][1])-upper_letter(b[1][0], b[1][1]), pow(len(alphabet), 2))
+    coefb=[]
+    for ael in coefa:
+        coefb.append((upper_letter(b[0][0], b[0][1])-ael*upper_letter(a[0][0], a[0][1]))%pow(len(alphabet), 2))
+    return [coefa, coefb]
+
+
+def sort_dict(a):
+    return {k: v for k, v in sorted(a.items(), key=lambda item: item[1])[::-1]}
+
+
+def is_plaintext(text):
+    frequencies = count_unigrams(text)
+    entr = entropy(frequencies)
+    frequencies = list(sort_dict(frequencies).keys())
+    if all([i in frequencies[:13] for i in ['о', 'а', 'е']]) and 4.15 <= entr <= 4.55:
+        return True
 
 
 
-
-f=open('text/V1', 'r')
-line=f.readlines()[0]
+f=open('text/12.txt', 'r')
+line=''.join(f.readlines()).replace('\n', '')
 x=count_bigrams(line, False)
-x={k: v for k, v in sorted(x.items(), key=lambda item: item[1])[::-1]}
+x=sort_dict(x)
 first_elements = [i[0] for i in list(x.items())[:5]]
 five_most_often = ["ст", "но", "то", "на", "ен"]
-#print(upper_letter("б", "а"))
-a = solve_linear(upper_letter(five_most_often[0][0], five_most_often[0][1])-upper_letter(five_most_often[1][0], five_most_often[1][1]), upper_letter(first_elements[0][0], first_elements[0][1])-upper_letter(first_elements[1][0], first_elements[1][1]), pow(len(alphabet), 2))
-b = (upper_letter(first_elements[0][0], first_elements[0][1])-a[0]*upper_letter(five_most_often[0][0], five_most_often[0][1]))%pow(len(alphabet), 2)
-#decipher_afin_bigrams(a[0], b, line)
-print(extended_euclid(133432,803429054))
+
+for i in itertools.permutations(five_most_often, 2):
+    for j in itertools.permutations(first_elements, 2):
+        a, b=get_coefs(i, j)
+        for (ael, bel) in zip(a, b):
+            plaintext = decipher_afin_bigrams(ael, bel, line)
+            if len(plaintext)>0 and is_plaintext(plaintext):
+                print(plaintext[:100])
+                #print(ael)
+                #print(bel)
+                #print(i)
+                #print(j)
